0844c2c4384f02958eb194eb1c1116e6d065973e
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2020-08-23 21:02:45 +0200

    More Test added for LogParser

--- spec/log_reports/log_parser_spec.rb
+++ spec/log_reports/log_parser_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe LogReports::LogParser do
     it 'creates an object of the described class' do
       expect(log_parser).to be_kind_of(described_class)
     end
+
+    it 'returns an error is there is NO log_path' do
+      expect { described_class.new }.to raise_error(ArgumentError)
+    end
   end
 
   describe '#to_webpages_info' do
@@ -20,6 +24,17 @@ RSpec.describe LogReports::LogParser do
       expect(result).to be_a_kind_of(LogReports::WebpagesInfo)
       expect(result.webpages.any?).to be(true)
       expect(result.webpages.first).to be_a_kind_of(LogReports::WebpageInfo)
+    end
+
+    describe '#parse (instance method)' do
+      it 'respond to method' do
+        expect(described_class).to respond_to(:parse)
+      end
+
+      it 'returns a WebpagesInfo' do
+        result = described_class.parse('spec/fixtures/test.log')
+        expect(result).to be_a_kind_of(LogReports::WebpagesInfo)
+      end
     end
   end
 end
